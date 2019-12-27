6cf322f619106b79a13c4e1fcaa3be2eea5b5512
Alex Buck <abuck@spotify.com>
2019-12-27 21:01:47 +0000

    optimise day 14 part a, part b

--- 2019/day14/day14_test.go
+++ 2019/day14/day14_test.go
@@ -7,36 +7,18 @@ import (
 
 func TestParseInput(t *testing.T) {
 	r := strings.NewReader("10 ORE => 10 A")
-	graph := parseInput(r)
+	equations := parseInput(r)
 
-	v := graph["A"]
+	checkIntEq(len(equations), 1, t)
 
-	checkIntEq(v.amountGeneratedByReac, 10, t)
-	checkIntEq(len(v.edges), 1, t)
+	eqn := equations[0]
+	checkIntEq(len(eqn.inputs), 1, t)
 
-	e := v.edges[0]
+	checkIntEq(eqn.inputs[0].amount, 10, t)
+	checkStrEq(eqn.inputs[0].name, "ORE", t)
 
-	checkIntEq(e.amount, 10, t)
-	checkStrEq(e.dest, "ORE", t)
-
-}
-
-func TestMultipleInputs(t *testing.T) {
-	r := strings.NewReader("11 QMDTJ, 15 LVPK, 5 LZPCS => 3 KJVZ")
-	graph := parseInput(r)
-
-	v := graph["KJVZ"]
-
-	checkIntEq(v.amountGeneratedByReac, 3, t)
-	checkIntEq(len(v.edges), 3, t)
-
-	checkStrEq(v.edges[0].dest, "QMDTJ", t)
-	checkStrEq(v.edges[1].dest, "LVPK", t)
-	checkStrEq(v.edges[2].dest, "LZPCS", t)
-
-	checkIntEq(v.edges[0].amount, 11, t)
-	checkIntEq(v.edges[1].amount, 15, t)
-	checkIntEq(v.edges[2].amount, 5, t)
+	checkIntEq(eqn.output.amount, 10, t)
+	checkStrEq(eqn.output.name, "A", t)
 
 }
 
