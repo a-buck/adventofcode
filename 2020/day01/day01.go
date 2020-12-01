155c268aceee787e9a6da0339839e02e74db1ebd
Alex Buck <alex.buck10@gmail.com>
2020-12-01 20:12:57 +0000

    day 1

--- 2020/day01/day01.go
+++ 2020/day01/day01.go
@@ -24,33 +24,56 @@ func main() {
 	}
 	defer file.Close()
 
-	ans := run(file, *partB)
+	vals := readVals(file)
+	ans := run(vals, *partB)
 
 	fmt.Printf("%d\n", ans)
-
 }
 
-func run(r io.Reader, partB bool) int {
-	const target = 2020
-
-	vals := make(map[int]int)
+func readVals(r io.Reader) []int {
+	vals := make([]int, 0)
 
 	scanner := bufio.NewScanner(r)
 
-	// populate vals with value being amount needed to reach target
 	for scanner.Scan() {
 		val, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		vals[val] = target - val
+		vals = append(vals, val)
+	}
+	return vals
+
+}
+
+func run(vals []int, partB bool) int {
+	const target = 2020
+
+	valsToIndex := make(map[int]int)
+	for i, v := range vals {
+		valsToIndex[v] = i
 	}
 
-	for k, v := range vals {
-		_, ok := vals[v]
+	if partB {
+		for i, v1 := range vals {
+			for j, v2 := range vals[i+1:] {
+				v3 := target - v1 - v2
+				k, ok := valsToIndex[v3]
+				if ok && i != k && j != k {
+					return v1 * v2 * v3
+				}
+			}
+		}
 
-		if ok {
-			return k * v
+	} else {
+		// part A
+
+		for i, v1 := range vals {
+			v2 := target - v1
+			j, ok := valsToIndex[v2]
+			if ok && i != j {
+				return v1 * v2
+			}
 		}
 	}
 
